256dd97d50839c7385ea00a5cd0fef6d0bdd1b96
bakhtiar-abid <abid.bu16@gmail.com>
2023-05-23 15:36:17 +0600

    Step Definitions for Login Feature

---
+++ src/test/steps/loginStep.ts
@@ -0,0 +1,220 @@
+// 1) Scenario: Navigation to the login page 
+  
+import { Given, When, Then } from "@cucumber/cucumber"; 
+
+         Given('user navigates to the application home page', async function () {
+           
+         });
+
+   
+
+         When('user click on my login page', async function () {
+           
+         });
+
+ 
+       
+
+         When('user gets the title of the page', async function () {
+           
+         });
+
+   
+       
+
+         Then('page title should be {string}', async function (string) {
+           
+         });
+
+
+// 2) Scenario: Login with valid Username and invalid password
+       
+
+         Given('user click on my login page', async function () {
+           
+         });
+
+ 
+       
+
+         When('user enters username {string}', async function (string) {
+           
+         });
+
+  
+       
+
+         When('user enters password {string}', async function (string) {
+           
+         });
+
+ 
+       
+
+         When('user clicks on Login button', async function () {
+           
+         });
+
+  
+       
+
+         Then('user should see error message {string}', async function (string) {
+           
+         });
+
+
+// 3) Scenario: Login with incorrect credentials 
+       
+
+         Given('user click on my login page', async function () {
+           
+         });
+
+
+       
+
+         When('user enters username {string}', async function (string) {
+           
+         });
+
+  
+       
+
+         When('user enters password {string}', async function (string) {
+           
+         });
+
+  
+       
+
+         When('user clicks on Login button', async function () {
+           
+         });
+
+  
+       
+
+         Then('user should see error message {string}', async function (string) {
+           
+         });
+
+
+// 4) Scenario: Login without giving any credentials 
+       
+
+         Given('user click on my login page', async function () {
+           
+         });
+
+  
+       
+
+         When('user enters username {string}', async function (string) {
+           
+         });
+
+  
+       
+
+         When('user enters password {string}', async function (string) {
+           
+         });
+
+
+       
+
+         When('user clicks on Login button', async function () {
+           
+         });
+
+  
+       
+
+         Then('user should see error message as {string}', async function (string) {
+           
+         });
+
+
+// 5) Scenario: Login with wrong email in Email field 
+       
+
+         Given('user click on my login page', async function () {
+           
+         });
+
+  
+       
+
+         When('user enters username {string}', async function (string) {
+           
+         });
+
+ 
+       
+
+         When('user enters password {string}', async function (string) {
+           
+         });
+
+ 
+       
+
+         When('user clicks on Login button', async function () {
+           
+         });
+
+ 
+
+         Then('user should see error message {string}', async function (string) {
+           
+         });
+
+
+// 6) Scenario: Login with correct credentials 
+       
+
+         Given('user click on my login page', async function () {
+           
+         });
+
+
+       
+
+         When('user enters username {string}', async function (string) {
+           
+         });
+
+
+       
+
+         When('user enters password {string}', async function (string) {
+           
+         });
+
+  
+       
+
+         When('user clicks on Login button', async function () {
+           
+         });
+
+  
+       
+
+         When('user click on my account page', async function () {
+           
+         });
+
+   
+       
+
+         When('user gets the title of the page', async function () {
+           
+         });
+
+   
+       
+
+         Then('page title should be {string}', async function (string) {
+           
+         });
