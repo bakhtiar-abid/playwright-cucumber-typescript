8f205c872ba6c4e118553e6fa5316b585a8af67c
bakhtiar-abid <abid.bu16@gmail.com>
2023-05-25 15:14:11 +0600

    New Build

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: "https://training.nop-station.com/",
-    headless: false,
+    headless: true,
     screenshot: "on",
     video: "on",
     // launchOptions:{
