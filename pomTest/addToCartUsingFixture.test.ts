762247c90be788902ea73544d26ab8577151e1ba
bakhtiar-abid <abid.bu16@gmail.com>
2023-05-25 23:52:16 +0600

    New Build with Runner

--- pomTest/addToCartUsingFixture.test.ts
+++ pomTest/addToCartUsingFixture.test.ts
@@ -1,7 +1,7 @@
 import { expect, test} from "../base/pomFixture";
 
 
-const email = "nmfdsafhjhj@gmail.com";
+const email = "new_user@gmail.com";
 const password = "123456";
 const confirmPass = "123456";
 
