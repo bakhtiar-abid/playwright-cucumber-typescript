6a50a2f2aa27984be3fa1237e8ef76385ba24c00
bakhtiar-abid <abid.bu16@gmail.com>
2023-06-15 11:57:34 +0600

    New Changes

--- pomTest/addToCartUsingFixture.test.ts
+++ pomTest/addToCartUsingFixture.test.ts
@@ -1,7 +1,7 @@
 import { test, addToTestResults, getTestResults } from '../base/pomFixture'
 import { expect, TestInfo, FullConfig, FullProject } from '@playwright/test'
 
-const email = 'nkhnjhj789898878@gmail.com'
+const email = 'hello_world@gmail.com'
 const password = '123456'
 const confirmPass = '123456'
 
